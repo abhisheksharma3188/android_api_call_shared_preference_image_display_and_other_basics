2c903b303ff20f0319448b3439f88808244b77a1
abhisheksharma3188 <111152006+abhisheksharma3188@users.noreply.github.com>
2023-06-09 11:16:56 +0530

    Create api.php

---
+++ api.php
@@ -0,0 +1,25 @@
+<?php
+    header('content-type:application/json');
+    if(isset($_REQUEST['number1'])){
+        $number1=$_REQUEST['number1'];
+    }else{
+        $number1=0;
+    }
+    if(isset($_REQUEST['number2'])){
+        $number2=$_REQUEST['number2'];
+    }else{
+        $number2=0;
+    }
+    //$multiplication=$number1*$number2;
+    //$responseArray=['multiplication'=>$multiplication,'people'=>[['name'=>'Abhishek','age'=>"35"],['name'=>'Chetana','age'=>"32"],['name'=>'Aadhvik','age'=>"4"]]];
+    if($number1==$number2){
+        $responseArray=[['name'=>'Abhishek','age'=>"35"],['name'=>'Chetana','age'=>"32"],['name'=>'Aadhvik','age'=>"4"]];
+    }else if($number1>$number2){
+        $responseArray=[['name'=>'Sumit','age'=>"35"],['name'=>'Prashant','age'=>"32"],['name'=>'Pushpendra','age'=>"4"]];
+    }else{
+        $responseArray=[['name'=>'Akash','age'=>"35"],['name'=>'Shanil','age'=>"32"],['name'=>'Meenal','age'=>"4"]];
+    }
+    
+    $responseJSON = json_encode($responseArray);
+    echo $responseJSON;
+?>
